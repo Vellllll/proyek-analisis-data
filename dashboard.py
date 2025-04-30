05988927e216275ace8eb0fb691adbbcdd28f2f9
arvel <arvelbudion01@gmail.com>
2025-04-30 22:35:32 +0700

    add dashboard to visualize the data

---
+++ dashboard.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sum_products_df = pd.read_csv('https://raw.githubusercontent.com/Vellllll/proyek-analisis-data/main/sum_products_df.csv')
+sum_sellers_df = pd.read_csv('https://raw.githubusercontent.com/Vellllll/proyek-analisis-data/main/sum_sellers_df.csv')
+
+st.header('Brazilian E-commerce Dashboard :sparkles:')
+st.subheader('Products distribution')
+
+product_options = st.radio(
+    label = 'Urutkan berdasarkan jumlah penjualan produk',
+    options = ('Tertinggi', 'Terendah'),
+    horizontal = True
+)
+
+fig, ax = plt.subplots(figsize = (20,10))
+
+sns.barplot(
+    x = 'product_id',
+    y = 'product_category_name_english',
+    data = sum_products_df.sort_values(by = 'product_id', ascending = False).head(10) if product_options == 'Tertinggi' else sum_products_df.sort_values(by = 'product_id', ascending = True).head(10),
+    ax = ax
+)
+
+ax.tick_params(axis = 'x', labelsize = 30)
+ax.tick_params(axis = 'y', labelsize = 30)
+ax.set_title('Products distribution by its category', loc = 'center', fontsize = 30)
+st.pyplot(fig)
+
+st.subheader('Sellers performance')
+
+sellers_options = st.radio(
+    label = 'Urutkan berdasarkan jumlah omset seller',
+    options = ('Tertinggi', 'Terendah'),
+    horizontal = True
+)
+
+col1, col2, col3 = st.columns(3)
+
+# with col1:
+fig, ax = plt.subplots(figsize = (20,10))
+
+sns.barplot(
+    x = 'price',
+    y = 'seller_city',
+    data = sum_sellers_df.sort_values(by = 'price', ascending = False).head(10) if sellers_options == 'Tertinggi' else sum_sellers_df.sort_values(by = 'price', ascending = True).head(10),
+    ax = ax
+)
+
+ax.tick_params(axis = 'x', labelsize = 30)
+ax.tick_params(axis = 'y', labelsize = 30)
+ax.set_title('Omset produk', loc = 'center', fontsize = 30)
+st.pyplot(fig)
+
+# with col2:
+fig, ax = plt.subplots(figsize = (20,10))
+
+sns.barplot(
+    x = 'product_id',
+    y = 'seller_city',
+    data = sum_sellers_df.sort_values(by = 'product_id', ascending = False).head(10) if sellers_options == 'Tertinggi' else sum_sellers_df.sort_values(by = 'product_id', ascending = True).head(10),
+    ax = ax
+)
+
+ax.tick_params(axis = 'x', labelsize = 30)
+ax.tick_params(axis = 'y', labelsize = 30)
+ax.set_title('Jumlah penjualan produk', loc = 'center', fontsize = 30)
+st.pyplot(fig)
+
+# with col3:
+fig, ax = plt.subplots(figsize = (20,10))
+
+sns.barplot(
+    x = 'order_item_id',
+    y = 'seller_city',
+    data = sum_sellers_df.sort_values(by = 'order_item_id', ascending = False).head(10) if sellers_options == 'Tertinggi' else sum_sellers_df.sort_values(by = 'order_item_id', ascending = True).head(10),
+    ax = ax
+)
+
+ax.tick_params(axis = 'x', labelsize = 30)
+ax.tick_params(axis = 'y', labelsize = 30)
+ax.set_title('', loc = 'center', fontsize = 30)
+st.pyplot(fig)
+
